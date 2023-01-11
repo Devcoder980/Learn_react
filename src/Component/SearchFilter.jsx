f0aa51be590726873938d96c86e1e077a1146d4b
░▒▓ Prabhudev Bind ▓▒░ <pbind4545@gmail.com>
2023-01-11 21:08:27 +0530

    Some use Tailwing css

--- src/Component/SearchFilter.jsx
+++ src/Component/SearchFilter.jsx
@@ -1,41 +1,50 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 import Post from '../source/Posts.json'
 
 function SearchFilter() {
     const [item, setitem] = useState('')
-    const handleclick=event=>{
-        console.log("Asign value"+item);
+    const handleclick = event => {
+        console.log("Asign value" + item);
         setitem(event.target.value)
     }
-    const searchfilter=Post.filter((story)=>{
-        return(story.body.includes(item));
+    const searchfilter = Post.filter((story) => {
+        return (story.body.includes(item));
     })
-  return (
-    <div>
-        <h1>Practice Make Searc</h1>
-        <Search onsearchclick={handleclick}/>
-        <Lists list={searchfilter}/>
-    </div>
-  )
+    return (
+        <div >
+            <h1>Practice Make Searc</h1>
+            <Search values={item} onsearchclick={handleclick} />
+            <div className='flex flex-wrap '>
+            <Lists  list={searchfilter} />
+            </div>
+            
+        </div>
+    )
 }
-const Search=props=>{
-    return(
+const Search = props => {
+    return (
         <div>
             <label htmlFor="Search">Search: </label>
-            <input type="text" className='border-2' id='Search' onChange={props.onsearchclick} />
+            <input type="text" className='border-2' value={props.values} id='Search' onChange={props.onsearchclick} />
         </div>
     )
 }
 
-const Lists=props=>{
-    return props.list.map(item=>(
-        <div className="flex border-2 bg-green-500" key={item.id}>
-            <div className="flex-1">
-                <div><h1>{item.title}</h1></div>
-            </div>
-            <div className="flex-1">
-                <dir><p>{item.body}</p></dir>
+const Lists = props => {
+    return props.list.map(item => (
+        <div class="flex justify-center m-4">
+            <div class="rounded-lg shadow-lg bg-white max-w-sm">
+                <a href="#!">
+                    <img class="rounded-t-lg" src={item.img} alt="" />
+                </a>
+                <div class="p-6">
+                    <h5 class="text-gray-900 text-xl font-medium mb-2">{item.title}</h5>
+                    <p class="text-gray-700 text-base mb-4">
+                        {item.body}
+                    </p>
+                    <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+                </div>
             </div>
         </div>
     ))
--- src/Component/StateManga.jsx
+++ src/Component/StateManga.jsx
@@ -32,8 +32,12 @@ function StateManga() {
             <p className="my-3">
                 Searching for <strong>{searchTerm}</strong>
             </p>
-            <button onClick={() => { setcount(count+1); }} className='border-1 rounded-sm px-3 bg-emerald-400 text-white'>Add</button>
-            <button onClick={() => { setcount(count -1) }} className='border-1 rounded-sm px-3 bg-emerald-400 text-white'>Sub</button>
+            <div class="flex space-x-2 justify-between">
+                <div>
+                    <button type="button"  onClick={()=>{setcount(count+1)}} class="inline-block px-6 py-2.5 bg-blue-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-lg hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Increment</button>
+                    <button type="button" onClick={()=>{setcount(count-1)}} class="inline-block px-6 py-2.5 bg-purple-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-lg hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Decrese</button>
+                </div>
+            </div>
             <label htmlFor="" className="mx-5">Value:</label><span>{count}</span>
         </div>
     )
