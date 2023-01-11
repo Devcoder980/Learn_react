cb50032833b35910f749484f26a439570ef8817a
░▒▓ Prabhudev Bind ▓▒░ <pbind4545@gmail.com>
2023-01-11 19:36:27 +0530

    Praclice Searchfilter completed

--- src/Component/SearchFilter.jsx
+++ src/Component/SearchFilter.jsx
@@ -1,49 +1,43 @@
-import React, { useState } from 'react'
+import React,{useState} from 'react'
+
 import Post from '../source/Posts.json'
 
-// Post.map(item=>{
-//     console.log(item.body)
-// })
-
 function SearchFilter() {
-
-    const [searchItem, setsearchItem] = useState('');
-    const handleSearch = event => {
-        setsearchItem(event.target.value);
-    };
-    
-    console.log("hello");
-    const searchedStories = Post.filter(function(story) {
-        return story.title.includes(searchItem);
-    });
-    return (
-        <div className='border-2 bg-yellow-500 my-6'>
-            <Search onSearch={handleSearch} />
-            <h1>Value of List</h1>
-            <h1>{searchItem}</h1>
-            <Lists todolist={searchedStories} />
-        </div>
-
-    )
+    const [item, setitem] = useState('')
+    const handleclick=event=>{
+        console.log("Asign value"+item);
+        setitem(event.target.value)
+    }
+    const searchfilter=Post.filter((story)=>{
+        return(story.body.includes(item));
+    })
+  return (
+    <div>
+        <h1>Practice Make Searc</h1>
+        <Search onsearchclick={handleclick}/>
+        <Lists list={searchfilter}/>
+    </div>
+  )
 }
-
-const Search = props => {
-    return (
-
+const Search=props=>{
+    return(
         <div>
-            <label htmlFor="search">Search:</label>
-            <input type="text" onChange={props.onSearch} />
+            <label htmlFor="Search">Search: </label>
+            <input type="text" className='border-2' id='Search' onChange={props.onsearchclick} />
         </div>
     )
 }
 
-const Lists = props => {
-    return props.todolist.map(item => (
-        <div className="flex border-blue-800 my-2" key={item.id} >
-            <h1 className='flex-1 text-lg'>{item.title}</h1>
-             <p className='flex-1 text-sm border-2 border-l-blue-800'>{item.body}</p>
+const Lists=props=>{
+    return props.list.map(item=>(
+        <div className="flex border-2 bg-green-500" key={item.id}>
+            <div className="flex-1">
+                <div><h1>{item.title}</h1></div>
+            </div>
+            <div className="flex-1">
+                <dir><p>{item.body}</p></dir>
+            </div>
         </div>
     ))
-
 }
 export default SearchFilter
