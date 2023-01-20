7fd03c7df2ff7e65736edeb7dfcf1dbbcbcb9964
░▒▓ Prabhudev Bind ▓▒░ <pbind4545@gmail.com>
2023-01-20 09:50:27 +0530

    react pass list funtion as componet

--- src/Component/RevistionDay.jsx
+++ src/Component/RevistionDay.jsx
@@ -1,61 +1,51 @@
-import { list } from 'postcss'
 import React from 'react'
 
-// const avenger=['Iron Man','captain American', 'Hulk','Thor','HawkEye'];
-const avenger=[
+const list = [
     {
-        id:1,
-        name:'Iron Man'
+        "_id": "UMJX3B72S28Q88V2",
+        "name": "Loan Rivers",
+        "dob": "2015-02-03",
+        "telephone": "+40-0104-119-444",
+        "score": 2.4,
+        "email": "myrnadangelo3@limitation.com",
+        "url": "https://www.kit.com",
+        "description": "permalink continuously flame gratis plains ideas knew induction adjust due podcasts surrounded performance support childhood limited monte feel acceptable oriented",
+        "verified": true,
+        "salary": 10864
     },
     {
-        id:2,
-        name:'capitain America'
-    },
-    {
-        id:3,
-        name:'Harsh'
-    },
-    {
-        id:4,
-        name:'Devendra'
-    },
-    {
-        id:5,
-        name:'Akhilesh'
+        "_id": "DEETNUGE1CCKQB4K",
+        "name": "Inocencia Pinkerton",
+        "dob": "2017-04-19",
+        "telephone": "+48-4321-749-834",
+        "score": 5.4,
+        "email": "maciehogan@gmail.com",
+        "url": "https://www.surveys.com",
+        "description": "they alike white potatoes radius additionally nw attention expects partnerships assessment sponsored affiliation exports intent childrens automation cinema canvas youth",
+        "verified": true,
+        "salary": 32732
     }
-
-];
-// let aven=avenger.map(ave=><li>{ave}</li>)
-
-const a=avenger.map((ave)=>{
-    console.log(ave.name)
-    return <li key={ave.id}>ave.name</li>
-})
-console.table(a)
-const avens=avenger.map(ave=>(<li key={ave.id}>{ave.name}</li>) )
-
-console.table(avens)
-
+]
 export const RevistionDay = () => {
-  return (
-    <div>
-        <h1 className="text-center">My Avenger LIst</h1>
-        {/* <ul className="bg-blue-500 text-white text-3xl">
-            {avenger.map(avenger=><li>{avenger}</li>)}
-        </ul>
-        <ul>
-            {aven}
-        </ul> */}
-
-        <ul>
-            {avens}
-        </ul>
-
-        <p>
-            {avenger.map(function(item){
-                return <div>{item.name}</div>
-            })}
-        </p>
-    </div>
-  )
+    return (
+        <div>
+            <List />
+        </div>
+    )
+}
+function List(){
+    return list.map(function(item){
+        return(
+            <div key={item._id}>
+                <span>
+                   <b><a href={item.url}>{item.name}</a></b> 
+                </span>
+                <br />
+                <span>{item.description}</span>
+                <br />
+                <span>{item.salary}</span>
+                <hr />
+            </div>
+        )
+    })
 }
