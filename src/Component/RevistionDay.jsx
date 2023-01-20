61620bf7ac12e47707a6deb37b962ee5e2e95481
░▒▓ Prabhudev Bind ▓▒░ <pbind4545@gmail.com>
2023-01-20 10:13:08 +0530

    react some object orinted programming

--- src/Component/RevistionDay.jsx
+++ src/Component/RevistionDay.jsx
@@ -1,51 +1,21 @@
+//Some Practive React class base component
+
 import React from 'react'
 
-const list = [
-    {
-        "_id": "UMJX3B72S28Q88V2",
-        "name": "Loan Rivers",
-        "dob": "2015-02-03",
-        "telephone": "+40-0104-119-444",
-        "score": 2.4,
-        "email": "myrnadangelo3@limitation.com",
-        "url": "https://www.kit.com",
-        "description": "permalink continuously flame gratis plains ideas knew induction adjust due podcasts surrounded performance support childhood limited monte feel acceptable oriented",
-        "verified": true,
-        "salary": 10864
-    },
-    {
-        "_id": "DEETNUGE1CCKQB4K",
-        "name": "Inocencia Pinkerton",
-        "dob": "2017-04-19",
-        "telephone": "+48-4321-749-834",
-        "score": 5.4,
-        "email": "maciehogan@gmail.com",
-        "url": "https://www.surveys.com",
-        "description": "they alike white potatoes radius additionally nw attention expects partnerships assessment sponsored affiliation exports intent childrens automation cinema canvas youth",
-        "verified": true,
-        "salary": 32732
+class Developer{
+    constructor(firstname,lastname){
+        this.firstname=firstname;
+        this.lastname=lastname;
     }
-]
-export const RevistionDay = () => {
-    return (
-        <div>
-            <List />
-        </div>
-    )
+    getName(){
+        return this.firstname + "  "+ this.lastname;
+    }
 }
-function List(){
-    return list.map(function(item){
-        return(
-            <div key={item._id}>
-                <span>
-                   <b><a href={item.url}>{item.name}</a></b> 
-                </span>
-                <br />
-                <span>{item.description}</span>
-                <br />
-                <span>{item.salary}</span>
-                <hr />
-            </div>
-        )
-    })
+
+const dev=new Developer("Dev",'Coder');
+console.table(dev.getName())
+export const RevistionDay = () => {
+  return (
+    <div>{dev.getName()}</div>
+  )
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React from "react";
+
 import {createRoot} from 'react-dom/client'
-import App from './App.js';
+import App from './App';
 
-
-const root=document.getElementById('root');
-createRoot(root).render(<App/>)
+const root=createRoot(document.getElementById("root"));
+root.render(<App/>)
