a6a1739934038b34dcf19ad490039a544515f2da
░▒▓ Prabhudev Bind ▓▒░ <pbind4545@gmail.com>
2023-01-27 23:08:49 +0530

    nothing do

--- src/Component/Composition.jsx
+++ src/Component/Composition.jsx
@@ -1,22 +1,5 @@
 import React from 'react';
-const stories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
+
   const initialStories = [
     {
       title: 'React',
