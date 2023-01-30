f4a9b12f132a7f1862a24aaa4781b5e12f796a65
░▒▓ Prabhudev Bind ▓▒░ <pbind4545@gmail.com>
2023-01-30 17:59:51 +0530

    complete

--- src/Component/Forms.jsx
+++ src/Component/Forms.jsx
@@ -1,32 +1,43 @@
 import React from 'react'
+import { useState } from 'react';
+// Rendering lists 
+
+function MyButton({count,onClick}){
+    return(
+        <button className='border-2' onClick={onClick}>Click {count}</button>
+
+    )
+} 
 
 function Forms() {
-    let content;
-    let isLoggidIn=false;
-    if(isLoggidIn){
-        content=<h1>Hello Wrold</h1>;;
+    const [count,setCount]=useState(0);
+    function handleClick(){
+        setCount(count+1);
     }
-    else{
-        content=<h1>Welcome Dev</h1>;
-    }
-    
+    const product=[
+        {title:'cabbage',isFruit:false,id:1},
+        {title:'GArlic',isFruit:false,id:2},
+        {title:'Apple',isFruit:true,id:3},
+    ];
+
+    const listItem=product.map(product=>
+        <li key={product.id} style={{color:product.isFruit ? 'magenta' :'darkgreen'}}>
+            {product.title}
+        </li>);
+
+
   return (
     <div>
-        {content}
-    <div>
-    {
-            isLoggidIn ? (
-                content
-            ):(
-                content
-            )
-        }prabh
-    </div>
-        <p>
-            {isLoggidIn && content}
-        </p>
+        <ul>
+            {listItem}
+        </ul>
+        <span>
+            <MyButton count={count} onClick={handleClick}/>
+            <MyButton count={count} onClick={handleClick}/>
+        </span>
     </div>
   )
 }
+
 
 export default Forms
